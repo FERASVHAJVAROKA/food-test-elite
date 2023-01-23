56a892e918dcb812d1d0b8105943d9815ba5041a
FERASVHAJVAROKA <116260001+FERASVHAJVAROKA@users.noreply.github.com>
2023-01-23 11:40:59 +0100

    Add files via upload

---
+++ src/components/common/Logo/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.png';
+import './styles.css';
+
+const Logo = () => (
+  <div className='header-content_logo'>
+
+    <p>
+      <b>Restaurant Elite-Food</b>
+    </p>
+  </div>
+);
+
+export default Logo;
